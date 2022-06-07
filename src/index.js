edab6d2b9c2b53f6292f8fae25cc11c71d5235ab
Max Meischke <meischkem@outlook.com>
2022-06-07 14:27:03 -0400

    cleared test script

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
 import './style.css';
 
-function test(){
-    const testDiv = document.createElement('div');
-    testDiv.textContent = " Working!"
-    return testDiv;
-}
-
-document.body.appendChild(test());
