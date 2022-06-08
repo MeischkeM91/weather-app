a2281a19ad4dc039c4996eeb85a02c4af9a371fe
Max Meischke <meischkem@outlook.com>
2022-06-08 16:48:18 -0400

    Created importicons.js to isolate import of icons

---
+++ src/importicons.js
@@ -0,0 +1,8 @@
+import CloudyIcon from './icons/cloudy.svg';
+import DrizzleIcon from './icons/drizzle.svg';
+import HazeFogIcon from './icons/haze-fog.svg';
+import RainIcon from './icons/rain.svg';
+import SnowIcon from './icons/snow.svg';
+import SunnyIcon from './icons/sunny.svg';
+import ThunderIcon from './icons/thunder.svg';
+export {CloudyIcon,DrizzleIcon,HazeFogIcon,RainIcon,SnowIcon,SunnyIcon,ThunderIcon};
